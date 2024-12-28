8370e60b55a243486ad88c0dc281be02624036b4
m-h-d-salih <salihsha656@gmail.com>
2024-12-28 18:58:35 +0530

    set up mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 const app = express();
 dotenv.config();
@@ -9,6 +10,12 @@ const port = process.env.PORT  || 4000;
 app.get('/', (req, res) => {
   res.send('Hello, TypeScript + Node.js + Express!');
 });
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+console.log(`connected to mongodb`)
+}
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
