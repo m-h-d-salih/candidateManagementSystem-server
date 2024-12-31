ba569b1f1654c25321b1a691d08ea50c02cee39a
m-h-d-salih <salihsha656@gmail.com>
2024-12-31 11:10:51 +0530

    s3 profile

--- src/routes/candidateRoutes.ts
+++ src/routes/candidateRoutes.ts
@@ -3,7 +3,10 @@ import { createValidator } from 'express-joi-validation';
 import { trycatch } from '../middlewares/tryCatch';
 import { login } from '../controllers/candidate/authController';
 import { candidateLoginValidation } from '../middlewares/validation/candidate/authValidation';
+import uploadImage from '../middlewares/uploadImage';
+import { uploadProfileImage } from '../controllers/candidate/profileController';
 const router=express.Router();
 const validator=createValidator({passError:true});
 router.post('/login',validator.body(candidateLoginValidation),trycatch(login))
+router.put('/upload/:id',uploadImage.single('image'),trycatch(uploadProfileImage))
 export default router;
